2ca9937fff07225c53c0855e9cd1428f3f583b3e
serref jaguraga <reef163@gmail.com>
2022-01-30 22:42:21 -0500

    commit tree fc1be01ed46d4ac6cb1e4f5e9793165d59b7621f

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1
+let num2 = 62
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+
+let max = Math.max(5, 10, 15, 20);
